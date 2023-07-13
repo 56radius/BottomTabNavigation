f7602413d465be8af7d4efaf6cbd85dcee83abd9
56radius <cussmeritmohammed@gmail.com>
2023-07-13 19:56:38 +0100

    Update

--- App.js
+++ App.js
@@ -1,20 +1,108 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import * as React from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { Text, View, ScrollView } from "react-native";
+import { AntDesign, Zocial, Ionicons } from "@expo/vector-icons";
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+{
+  /* Home Screen */
+}
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text> Welcome to Home screen </Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+{
+  /* Search Screen */
+}
+function SearchScreen() {
+  <View>
+    <Text> Welcome to search screen </Text>
+  </View>;
+}
+
+{
+  /* post screen */
+}
+function PostScreen() {
+  <View>
+    <Text>Welcome To Posts</Text>
+  </View>;
+}
+
+{
+  /* Reels screen */
+}
+function ReelsScreen() {
+  <View>
+    <Text> Welcome To Reels </Text>
+  </View>;
+}
+
+{
+  /* profile screen */
+}
+function ProfileScreen() {
+  <View>
+    <Text> Welcome To Profile </Text>
+  </View>;
+}
+
+export default function MyTabs() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          options={{
+            tabBarIcon: () => (
+              <Ionicons name="ios-home-outline" size={24} color="black" />
+            ),
+          }}
+          name="Home"
+          component={HomeScreen}
+        />
+        <Tab.Screen
+          options={{
+            tabBarIcon: () => (
+              <AntDesign name="search1" size={24} color="black" />
+            ),
+          }}
+          name="Search"
+          component={SearchScreen}
+        />
+        <Tab.Screen
+          options={{
+            tabBarIcon: () => (
+              <AntDesign name="plussquareo" size={24} color="black" />
+            ),
+          }}
+          name="Post"
+          component={PostScreen}
+        />
+        <Tab.Screen
+          options={{
+            tabBarIcon: () => (
+              <Zocial name="instagram" size={24} color="black" />
+            ),
+          }}
+          name="Reels"
+          component={ReelsScreen}
+        />
+        <Tab.Screen
+          options={{
+            tabBarIcon: () => (
+              <AntDesign name="profile" size={24} color="black" />
+            ),
+          }}
+          name="Profile"
+          component={ProfileScreen}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
